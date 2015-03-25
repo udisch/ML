0c9bde124e332553eb83017e2e04534861bab629
Udi S <udisch@gmail.com>
2015-03-25 21:06:58 +0200

    predict

--- ex2/predict.m
+++ ex2/predict.m
@@ -14,10 +14,9 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
-
-
-
-
+threshold = 0.5;
+prediction = sigmoid(X*theta);
+p = (prediction >= threshold);
 
 
 
