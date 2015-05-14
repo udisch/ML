414f8fb1b1a47d94d011fe663c8f961dd62d7dc6
Udi S <udisch@gmail.com>
2015-05-14 21:01:40 +0300

    find parameters

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -22,12 +22,22 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+C_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigma_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+min_err = intmax;
 
-
-
-
-
-
+for i=1:size(C_vec,2)
+	for j=1:size(sigma_vec,2)
+		model = svmTrain(X, y, C_vec(i), @(x1, x2) gaussianKernel(x1, x2, sigma_vec(j))); 
+		predictions = svmPredict(model, Xval);
+		err = mean(double(predictions ~= yval));
+		if (err < min_err)
+			C = C_vec(i);
+			sigma = sigma_vec(j);
+			min_err = err;
+		end
+	end
+end
 
 % =========================================================================
 
