a87f66e91cddeb6a9021b7d09bbdb9a0d019aecb
Udi <udisch@gmail.com>
2015-05-16 13:36:17 +0300

    find closest centroids

--- ex7/findClosestCentroids.m
+++ ex7/findClosestCentroids.m
@@ -20,11 +20,16 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
-
-
-
-
-
+for i=1:size(X,1)		
+	min_dist = intmax;
+	for j=1:K
+		dist = norm(X(i,:) - centroids(j,:)) .^ 2;
+		if (dist < min_dist)
+			idx(i) = j;
+			min_dist = dist;
+		end
+	end
+end
 
 
 % =============================================================
